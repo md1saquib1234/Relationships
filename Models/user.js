cba9b9e82b5cfcd881bdadbf6cb9d10f5a61c84c
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-03-13 17:50:00 +0530

    chore: add package.json with initial dependencies for express, mongoose, and node

---
+++ Models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+
+main().then(console.log("Connection successful")).catch(err => console.log(err));
+
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/relationDemo'}
