1dce6ee8c1cd4a58399e29eb3b0338d2dd1e78b1
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-03-14 22:30:44 +0530

    fix: add missing newline in user.js for better readability

--- Models/user.js
+++ Models/user.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 main().then(console.log("Connection successful")).catch(err => console.log(err));
 
 
+
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/relationDemo');
 }
