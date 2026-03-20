cf1dd384a77555d53b18497dafacb0150241e37a
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-03-20 23:42:39 +0530

    fix: remove unnecessary blank lines for better code formatting

--- Models/user.js
+++ Models/user.js
@@ -6,10 +6,6 @@ const Schema = mongoose.Schema;
 main().
  then(console.log("Connection successful")).catch(err => console.log(err));
 
-
-
-
- 
  async function main() {
   await mongoose.connect('mongodb://localhost:27017/relationDemo');
 }
