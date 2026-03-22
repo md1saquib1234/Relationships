3d7a493fafec22fc7eb7a491429bdb9b2d024b5f
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-03-22 10:32:40 +0530

    fix: add initial customer model implementation

---
+++ Models/customer.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+main().
+ then(console.log("Connection successful")).catch(err => console.log(err));
+
+ async function main() {
+  await mongoose.connect('mongodb://localhost:27017/relationDemo');
+}
