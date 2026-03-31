34ef0a05f74904ec02219001102395f931cdd74a
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-03-31 17:04:59 +0530

    Customer Schema

--- Models/customer.js
+++ Models/customer.js
@@ -14,15 +14,44 @@ const orderSchema = new Schema({
   price: Number,
 });
 
-const Order = mongoose.model("Order", orderSchema);
+const customerSchema = new Schema({
+  name: String,
+  orders: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Order"
+    }
+  ]
+});
 
-const addOrders = async () => {
-  let res = await Order.insertMany([
-     {item: "Samosa", price: 12},
-     {item: "Chips", price: 10},
-     {item: "Chocolate", price: 40}
-  ]);
-  console.log(res);
+
+const Order = mongoose.model("Order", orderSchema);
+const Customer = mongoose.model("Customer", customerSchema);
+
+const addCustomer = async () => {
+  let cust1 = new Customer({
+    name : "Rahul Kumar",
+  });
+
+  let order1 = await Order.findOne({item: "Chips"});
+  let order2 = await Order.findOne({item: "Chocolate"});
+
+  cust1.orders.push(order1);
+  cust1.orders.push(order2);
+
+  let result = await cust1.save();
+  console.log(result);
 };
 
-addOrders();
+addCustomer();
+
+// const addOrders = async () => {
+//   let res = await Order.insertMany([
+//      {item: "Samosa", price: 12},
+//      {item: "Chips", price: 10},
+//      {item: "Chocolate", price: 40}
+//   ]);
+//   console.log(res);
+// };
+
+// addOrders();
