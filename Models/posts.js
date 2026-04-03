d79e85a604b09392c2bab32bad7e3e1b9e5a135b
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-04-03 17:50:05 +0530

    posts

--- Models/posts.js
+++ Models/posts.js
@@ -27,26 +27,26 @@ const postSchema = new Schema({
 const User = mongoose.model("User", userSchema);
 const Post = mongoose.model("Post", postSchema);
 
-// const addData = async () => {
-//   let user = await User.findOne({username: "rahulkumar"});
+const addData = async () => {
+  let user = await User.findOne({username: "rahulkumar"});
 
-//   let post2 = new Post({
-//     content: "Bye Bye",
-//     likes: 23,
-//   });
+  let post2 = new Post({
+    content: "Bye Bye",
+    likes: 23,
+  });
 
-//   post2.user = user;
+  post2.user = user;
 
 
-//   await post2.save();
-// };
-
-// addData();
-
-const del = async () => {
-  await Post.findByIdAndDelete('69b93e081a278f8a16787906');
-  await User.findByIdAndDelete('69ce3b4ac053236d37d3bf06');
-
+  await post2.save();
 };
 
-del();
+addData();
+
+// const del = async () => {
+//   await Post.findByIdAndDelete('69b93e081a278f8a16787906');
+//   await User.findByIdAndDelete('69ce3b4ac053236d37d3bf06');
+
+// };
+
+// del();
