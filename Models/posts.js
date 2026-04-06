5d2140cc2cc595a7938f1b7cab445167f3ffc7d6
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-04-06 22:41:11 +0530

    Adding delete element

--- Models/posts.js
+++ Models/posts.js
@@ -43,10 +43,10 @@ const addData = async () => {
 
 addData();
 
-// const del = async () => {
-//   await Post.findByIdAndDelete('69b93e081a278f8a16787906');
-//   await User.findByIdAndDelete('69ce3b4ac053236d37d3bf06');
+const del = async () => {
+  await Post.findByIdAndDelete('69b93e081a278f8a16787906');
+  await User.findByIdAndDelete('69ce3b4ac053236d37d3bf06');
 
-// };
+};
 
-// del();
+del();
