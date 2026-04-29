e0f5ae9dedb9823892487b8e4838704cadce9040
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-04-29 18:23:51 +0530

    Adding Spaces

--- Models/user.js
+++ Models/user.js
@@ -40,16 +40,15 @@ const addUsers = async () => {
 
     user1.addresses.push({
       location: "456 Main st",
-
       city: "Paris"
     });
 
     await user1.save();
+
     let result = await user1.save();
     console.log(result);
 
 }
-
 
 addUsers();
 
